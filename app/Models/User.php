1447cc8958603daa60f77cba4a7d1960410098b7
hayder ali <hxhwe1924@gmail.com>
2021-03-01 15:26:17 +0300

    Procfile for Heroku

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,13 +20,5 @@ class User extends Authenticatable
     ];
 
    
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
    
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }
