1422fcc3c37b1be4cbc9704b0592c47992cf2ec9
hayder ali <hxhwe1924@gmail.com>
2021-03-01 14:14:30 +0300

    Procfile for Heroku

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -55,10 +55,10 @@ class AdminController extends BaseController
            $users=crypt::decrypt($user->password);
            if($users===$request->password && $user->name==$request->name){
            // if( Auth::guard('admin')->attempt(['name' => $request->name, 'password' => $request->password])){
-               // $user=Auth::user();
+                $userr=Auth::user();
           //  $success['token']=$user->createToken(';ejhih/><{+876yk')->accessToken;
     
-                return $this->Respone($user,$users);
+                return $this->Respone($userr,$users);
     
     
          } else{
